08b9d22dc601d8afabd5a88457e12ccbdb47148b
JuanTiseira <jtiseira7@gmail.com>
2023-12-05 12:24:32 -0300

    updated the menu and home layout

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Button, Menu, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-
+import { IconButton } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeMode, selectMode } from '@/features/user/userSlice';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 function SimpleMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const dispatch = useDispatch();
+  const mode = useSelector(selectMode);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -23,6 +28,17 @@ function SimpleMenu() {
       >
         Open Menu
       </Button>
+      <IconButton
+        onClick={() => dispatch(changeMode())}
+        sx={{ position: 'absolute', top: 20, right: 40 }}
+      >
+        <Brightness4Icon
+          sx={{
+            transition: 'transform 0.4s',
+            transform: mode === 'dark' ? 'rotateY(180deg)' : 'rotateY(0deg)',
+          }}
+        />
+      </IconButton>
       <Menu
         id="simple-menu"
         anchorEl={anchorEl}
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,30 +1,62 @@
 import React from 'react'
-import { Container, Stack} from '@mui/material'
+import { Container, Stack, Grid} from '@mui/material'
 import SimpleMenu from '@/components/Menu/Menu'
 const Dashboard = () => {
   return (
-    <Container>
-        <Stack
+    <Container fixed
+      sx={{
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        flexDirection: "column"
+      }}
+    >
+      <Container
         sx={{
             p: 2,
             boxShadow: 3,
-            borderRadius: 5,
-            position: 'relative',
-            backgroundColor: 'grey.100',
-          }}
-          gap={1}>
-            <SimpleMenu></SimpleMenu>
+            borderRadius: 2,
+            backgroundColor: 'grey.50',
+            margin:"10px",
+            }}
+      >
+        <SimpleMenu></SimpleMenu>
+      </Container>
+
+      <Container
+      sx={{
+        p: 2,
+        boxShadow: 3,
+        borderRadius: 2,
+        backgroundColor: 'grey.50',
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        flexDirection: "column",
+      }}>
+        <Stack 
+          sx={{
+              p: 2,
+              boxShadow: 3,
+              borderRadius: 2,
+              backgroundColor: 'grey.100',
+              color: "white",
+            }}>
+            Hola
         </Stack>
-        <Container 
-        sx={{
-            p: 2,
-            boxShadow: 3,
-            borderRadius: 5,
-            position: 'relative',
-            backgroundColor: 'grey.100',
-          }}>
-            
-        </Container>
+        <Stack 
+          sx={{
+              p: 2,
+              boxShadow: 3,
+              borderRadius: 2,
+              backgroundColor: 'grey.100',
+              color: "white",
+            }}
+        >
+          Hola
+        </Stack>
+      </Container>
+        
     </Container>
   )
 }
