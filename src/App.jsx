626e6c6b768444f34ef18f0a241be5fa0b14c530
JuanTiseira <jtiseira7@gmail.com>
2024-01-14 20:53:01 -0300

    added navbar with side options

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import Routing from './routes/Routing';
+import ButtonAppBar from './components/Navbar/navbar';
+import TemporaryDrawer from './components/Sidebar/sidebar';
 
 function App() {
   return (
     <BrowserRouter>
       <CssBaseline />
+      <ButtonAppBar/>
       <Routing />
     </BrowserRouter>
   );
